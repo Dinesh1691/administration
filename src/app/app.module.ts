7019e87beeb20dd49b7bde63165f345ee6722c0a
Dinesh <dinesh.np@terralogic.com>
2019-03-06 15:06:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BsModalService } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GlobalService } from './global.service';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [BsModalService,GlobalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
